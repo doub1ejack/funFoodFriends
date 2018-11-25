1945e65fff0d863d6bc042ecf22d00d51e9ae3e4
Micah Mutrux <micah.mutrux@gmail.com>
2018-11-25 10:17:00 -0500

    FORMS: bind form input to app state
    1. Created state object for the form inputs.
    2. Created generic onChange handler.
    3. Bind the form inputs to the handler & set value from state.

--- src/App.js
+++ src/App.js
@@ -3,20 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      currentItem: '',
+      username: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className='app'>
         <header>
             <div className='wrapper'>
               <h1>Fun Food Friends</h1>
-              
+
             </div>
         </header>
         <div className='container'>
           <section className='add-item'>
               <form>
-                <input type="text" name="username" placeholder="What's your name?" />
-                <input type="text" name="currentItem" placeholder="What are you bringing?" />
+                <input type="text"
+                  name="username"
+                  placeholder="What's your name?"
+                  onChange={this.handleChange}
+                  value={this.state.username}
+                />
+
+                <input type="text"
+                  name="currentItem"
+                  placeholder="What are you bringing?"
+                  onChange={this.handleChange}
+                  value={this.state.currentItem}
+                />
+
                 <button>Add Item</button>
               </form>
           </section>
